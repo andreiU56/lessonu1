a6f25e98fd6d96708a088ec23c55a81fd4382a68
andreiU56 <jsandreidev@gmail.com>
2024-06-04 13:09:39 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,28 @@
+
+my_list=[3,4,6, 'text1','orange',  34,434]
+print(my_list)
+print(my_list[0])
+print(my_list[5])
+sublist=my_list[4]
+print('first element:',my_list[0])
+print('last element:', my_list[6] )
+print(my_list[2:5])
+my_list[2]=989
+print(my_list)
+
+my_dict={ 'orange':12, 'banan':13, 'kivy':14, 'mango':15, 'Granat':16, 'password':450959873309656  }
+print(my_dict)
+print('pere' in my_dict)
+print(my_dict.get('kivy'))
+print(list(my_dict))
+print('values of Dictionar:',my_dict.values())
+my_dict['mango']=80000
+my_dict2={'kartoska':1000}
+my_dict.update(my_dict2)
+print(my_dict)
+
+# print(my_dict)
+# print(set())
+#
+# print(type(my_list))
+# print()
